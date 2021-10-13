4b0b87840e21fe5d9ff1cf022f8f3206d448bf0c
davvore33 <davvore33@gmail.com>
2021-10-13 21:47:45 +0200

    Create tests script

--- starter.sh
+++ starter.sh
@@ -1,7 +1,49 @@
 #!/usr/bin/env bash
 
-exec="cmake-build-debug/eglue_client"
+#set -e
 
-$exec --load /home/matteo/Documents/eGlue/eglue-server/try.ini
-$exec --get Section1.Value1
-$exec --set Section1.Value1 15
+eglue_client="cmake-build-debug/eglue_client"
+eglue_server="../eglue-server/cmake-build-debug/eglue_server"
+eglue_config="/home/matteo/Documents/eGlue/eglue-server/try.ini"
+
+pid_server=0
+server_port=12345
+
+fun run_server() {
+  $eglue_server &
+  error=$?
+  echo server error $?
+  pid_server=$!
+  echo "Server up and running with pid " $pid_server " on port " $server_port
+}
+
+func server_error() {
+
+  kill $pid_server
+}
+
+fun test1() {
+  run_server
+  ss -ltupn | grep $server_port
+  kill -SIGINT $pid_server
+  ss -ltupn | grep $server_port
+}
+
+fun test2() {
+  tmp_config="/tmp/config.ini"
+  cp $eglue_config $tmp_config
+
+  run_server
+
+  a="$($eglue_client --load ${tmp_config} 2>&1)"
+  a="$($eglue_client --get Section1.Value1 2>&1)"
+  a="$($eglue_client --get Section2.Value1 2>&1)"
+  a="$($eglue_client --set Section1.Value1 15 2>&1)"
+  echo client error ${a}
+
+  kill -SIGINT $pid_server
+}
+
+test1
+
+test2
